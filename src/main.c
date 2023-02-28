6dbe9543e29b4c5d3d6b9006442392f7a47cefeb
danieldinis007 <daniel.dinis007@gmail.com>
2023-02-28 18:58:05 +0000

    main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  printf("Hello world!\n");
+int main(int argc, char *argv[])
+{
+  printf("sssssss world!\n");
   return EXIT_SUCCESS;
 }
