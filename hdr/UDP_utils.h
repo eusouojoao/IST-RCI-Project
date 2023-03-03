9c5a184acc67b27ef1dea18314ef72fa3574b755
eusouojoao <81173835+eusouojoao@users.noreply.github>
2023-03-03 17:25:46 +0000

    *

--- hdr/UDP_utils.h
+++ hdr/UDP_utils.h
@@ -2,5 +2,6 @@
 #define UDP_utils_H
 
 #include "struct.h"
+void send_user_message_UDP (user_args *uip, char *msg);
 
 #endif /* ifndef UDP_utils_HUDP_utils_H */
--- src/UDP_utils.c
+++ src/UDP_utils.c
@@ -1,6 +1,32 @@
 #include "../hdr/UDP_utils.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-void function(char *msg) {
-  return;
+void send_user_message_UDP (user_args *uip, char *msg) {
+  int fd = -1;
+  struct sockaddr_in addr;
+
+  fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (fd == -1) {
+    /*error*/ exit(EXIT_FAILURE);
+  }
+
+  memset(&addr, 0, sizeof(addr));
+  if (inet_pton(AF_INET, uip->regIP, &(addr.sin_addr)) != 1) {
+    /*error*/ exit(EXIT_FAILURE);
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons((in_port_t)uip->regUDP);
+
+  // sendto(...);
+
+  // recvfrom(...);
+
+  close(fd);
   return;
 }
