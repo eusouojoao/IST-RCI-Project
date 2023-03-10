00c370623699aff58c6c42e384b719dc03f6dda2
danieldinis007 <daniel.dinis007@gmail.com>
2023-03-10 09:36:38 +0000

    Show functions

--- src/interface/show_module.h
+++ src/interface/show_module.h
@@ -1,6 +1,11 @@
 #ifndef SHOW_MODULES_H
 #define SHOW_MODULES_H
 
+#include "../common/struct.h"
 #include "user_commands.h"
+
+void show_topology(host *myhost);
+void show_names(host *myhost);
+void show_routes(host *myhost);
 
 #endif // !SHOW_MODULES_H
