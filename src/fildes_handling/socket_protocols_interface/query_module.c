a74bcb7e4e735e85afd16fccceeaa187e8122c97
eusouojoao <81173835+eusouojoao@users.noreply.github.com>
2023-03-16 00:44:56 +0000

    *

--- src/fildes_handling/socket_protocols_interface/query_module.c
+++ src/fildes_handling/socket_protocols_interface/query_module.c
@@ -7,12 +7,16 @@ int find_name(char *name, host *host) {
   // APAGAR- não sei se é suposto devolver o conteúdo caso seja
   // encontrado ou apenas uma msg a dizer q o conteudo existe
   // const char *FindName()
-  names *list_pointer = host->names_list;
-  if (check_name(name) == -1)
+  if (check_name(name) == -1) {
     return -1; // Falha, name demasiado longo
+  }
+
+  names *list_pointer = host->names_list;
   while (list_pointer != NULL) {
-    if (strcmp(list_pointer->name, name) == 0)
+    if (strcmp(list_pointer->name, name) == 0) {
       return 1; // Sucesso, name encontrado
+    }
+
     list_pointer = list_pointer->next;
   }
 
