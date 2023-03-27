bee29bebef32f40ca76e71c99eba339c224c43f6
eusouojoao <81173835+eusouojoao@users.noreply.github>
2023-03-27 01:35:18 +0100

    *

--- src/fildes_handling/user_interface/join_module.h
+++ src/fildes_handling/user_interface/join_module.h
@@ -3,10 +3,7 @@
 
 #include "../../common/struct.h"
 
-void check_uniqueness_of_ID(host *host, char *node_list, char (*ID)[64]);
-void assign_host_ID_and_network(host *host, const char *ID, const char *net);
 int join_network(char *buffer, host *host);
-char *fetch_extern_from_nodelist(char *nodelist);
 int djoin_network(char *buffer, host *host, int flag);
 
 #endif // !JOIN_MODULES_H
